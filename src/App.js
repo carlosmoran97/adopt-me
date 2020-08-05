948244bb6c4de9ff46b8a89f76c04c1ef29b1d12
Carlos Morán <moran_krlos@hotmail.com>
2020-08-05 13:31:14 -0600

    search for pets

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1 id="something-important">Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
